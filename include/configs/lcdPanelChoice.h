121e862b1c839e4fb7be7cfbca941e0532cc4674
U-INSPIRON5160\tkisky <tkisky@Inspiron5160.(none)>
2007-06-13 14:57:30 -0700

    add lcd_svga, change OKAYA_QVGA timings

--- include/configs/lcdPanelChoice.h
+++ include/configs/lcdPanelChoice.h
@@ -17,13 +17,14 @@
 #define DA800X480		6
 #define DA640X480		7
 #define DA240X320		8
-#define DA800X600		9
-#define DA1024X768		10
-#define HITACHI_92		11
-#define tovis_w			12
-#define DP480X320		13
-#define DP320X240		14
-#define DL122X32		15
+#define lcd_svga		9
+#define DA800X600		10
+#define DA1024X768		11
+#define HITACHI_92		12
+#define tovis_w			13
+#define DP480X320		14
+#define DP320X240		15
+#define DL122X32		16
 #endif
 
 #if DISPLAY_TYPE == HITACHI_QVGA
@@ -44,6 +45,8 @@
 #define CONFIG_EXTRA_ENV_SETTINGS "panel=sharp_vga" "\0"
 #elif DISPLAY_TYPE == DA800X480
 #define CONFIG_EXTRA_ENV_SETTINGS "panel=hitachi_wvga" "\0"
+#elif DISPLAY_TYPE == lcd_svga
+#define CONFIG_EXTRA_ENV_SETTINGS "panel=lcd_svga" "\0"
 #elif DISPLAY_TYPE == DA1024X768
 #define CONFIG_EXTRA_ENV_SETTINGS "panel=crt1024x768" "\0"
 #elif DISPLAY_TYPE == HITACHI_92
