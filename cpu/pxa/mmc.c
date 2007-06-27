1d5a38116d865f4016ba53ff590b17351278b244
U-INSPIRON5160\tkisky <tkisky@Inspiron5160.(none)>
2007-06-27 16:17:43 -0700

    -increase retry, add delay between retries

--- cpu/pxa/mmc.c
+++ cpu/pxa/mmc.c
@@ -758,7 +758,8 @@ int SDCard_test( void )
 			}
 		}
 		bRetry++;
-		if (bRetry > 100) {
+		udelay(1000);
+		if (bRetry > 1000) {
 			printf("response %02x %02x %02x %02x %02x %02x\n",resp[0],resp[1],resp[2],resp[3],resp[4],resp[5]);
 			return -ENODEV;
 		}
