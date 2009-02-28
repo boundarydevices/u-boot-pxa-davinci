43801386fe60f931b315b0150c40aee2656ee23e
U-tkisky-hp\tkisky <tkisky@tkisky-hp.(none)>
2009-02-27 19:51:58 -0700

    add better error recovery to i2c

--- cpu/arm926ejs/davinci/i2c.c
+++ cpu/arm926ejs/davinci/i2c.c
@@ -32,14 +32,77 @@
 #include <asm/arch/hardware.h>
 #include <asm/arch/i2c_defs.h>
 
-#define CHECK_NACK() \
-	do {\
-		if (tmp & (I2C_TIMEOUT | I2C_STAT_NACK)) {\
-			REG(I2C_CON) = 0;\
-			return(1);\
-		}\
-	} while (0)
+#define MAC_VARIABLE "ethaddr"
 
+#define GP_BANK0_OFFSET	0x10
+#define GP_BANK_LENGTH	0x28
+#define GP_DIR 0x00
+#define GP_OUT 0x04
+#define GP_SET 0x08
+#define GP_CLR 0x0C
+#define GP_IN  0x10
+#define GPIO_SCL 43
+#define GPIO_SDA 44
+
+static void gpio_set_val_dir(unsigned int gp, int val, int output)
+{
+	unsigned int mask = (1 << (gp & 0x1f));
+	unsigned int bank = (gp >> 5);
+	int base = DAVINCI_GPIO_BASE+GP_BANK0_OFFSET+(bank * GP_BANK_LENGTH);
+	int set_clear = base + (val ? GP_SET : GP_CLR);
+	unsigned int dir = REG(base + GP_DIR);
+	if (output) {
+		dir &= ~mask;
+		REG(set_clear) = mask;
+		REG(base + GP_DIR) = dir;
+	} else {
+		dir |= mask;
+		REG(base + GP_DIR) = dir;
+		REG(set_clear) = mask;
+	}
+}
+
+static int gpio_get_val(unsigned int gp)
+{
+	unsigned int bank = (gp >> 5);
+	int base = DAVINCI_GPIO_BASE+GP_BANK0_OFFSET+(bank * GP_BANK_LENGTH);
+	return (REG(base + GP_IN) >> (gp & 0x1f)) & 1;
+}
+/*
+ * return 0 if already free, 1 if freed, -1 if failed to free
+ */
+static int DoGpioFreeBus(void)
+{
+	int retval = 0;
+	gpio_set_val_dir(GPIO_SCL, 1, 0);	/* high, input */
+	gpio_set_val_dir(GPIO_SDA, 1, 0);	/* high, input */
+
+	/* Disable I2C, make gpios again */
+	REG(PINMUX1) &= ~(1 << 7);
+
+	if (gpio_get_val(GPIO_SDA) == 0) {
+		int i = 0;
+		/* SDA is low, toggling clock */
+		retval = 1;
+		do {
+			gpio_set_val_dir(GPIO_SCL, 0, 1);	/* low, output */
+			udelay(1000);
+			gpio_set_val_dir(GPIO_SCL, 1, 0);	/* high, input */
+			udelay(1000);
+			if (gpio_get_val(GPIO_SDA))
+				break;
+			i++;
+			if (i>=16) {
+				printf("Error could not make SDA high\r\n");
+				retval = -1;
+				break;
+			}
+		} while (1);
+	}
+	/* Enable I2C pin Mux */
+	REG(PINMUX1) |= (1 << 7);
+	return retval;
+}
 
 static int wait_for_bus(void)
 {
@@ -47,8 +110,9 @@ static int wait_for_bus(void)
 
 	REG(I2C_STAT) = 0xffff;
 
-	for (timeout = 0; timeout < 10; timeout++) {
-		if (!((stat = REG(I2C_STAT)) & I2C_STAT_BB)) {
+	for (timeout = 0; timeout < 20; timeout++) {
+		stat = REG(I2C_STAT);
+		if (!(stat & I2C_STAT_BB)) {
 			REG(I2C_STAT) = 0xffff;
 			return(0);
 		}
@@ -58,24 +122,75 @@ static int wait_for_bus(void)
 	}
 
 	REG(I2C_STAT) = 0xffff;
+	printf("%s: timeout %x, trying bus reset\n", __func__, stat);
+	if (stat & I2C_STAT_BB) {
+		REG(I2C_CON) = 0;
+		udelay(5000);
+		printf("toggling enable to hopefully clear busy\r\n");
+		REG(I2C_CON) = I2C_CON_EN;
+	}
+	if (REG(I2C_STAT) & I2C_STAT_BB) {
+		printf("resetting i2c\r\n");
+		i2c_init(CFG_I2C_SPEED, CFG_I2C_SLAVE);
+		if (DoGpioFreeBus() == 1) {
+			printf("%s: toggling scl freed bus\n", __func__);
+		}
+	}
 	return(1);
 }
 
+#define I2C_STAT_NACK_AL (I2C_STAT_NACK | I2C_STAT_AL)
 
 static int poll_i2c_irq(int mask)
 {
 	int	stat, timeout;
-
-	for (timeout = 0; timeout < 10; timeout++) {
+	int	sum_stat = 0;
+	int	stop = 0;
+	int	clear_mask;
+	mask |= I2C_STAT_NACK_AL;
+	clear_mask = mask & ~(I2C_STAT_RRDY | I2C_STAT_XRDY);
+	for (timeout = 0; timeout < 200; timeout++) {
 		udelay(1000);
 		stat = REG(I2C_STAT);
+		sum_stat |= stat;
 		if (stat & mask) {
-			return(stat);
+			if (stat & clear_mask)
+				REG(I2C_STAT) = stat & clear_mask;
+			if (stat & I2C_STAT_NACK_AL) {
+				stop = 1;
+				mask |= I2C_STAT_ARDY;
+				if (mask & I2C_STAT_XRDY)
+					REG(I2C_CON) |= I2C_CON_STP | (1 << 7); /* repeat mode */
+				if (mask & I2C_STAT_RRDY)
+					REG(I2C_CON) |= I2C_CON_STP | (1 << 7) | (1 << 15); /* NACK */
+			}
+			if (stop) {
+				if (stat & I2C_STAT_RRDY){
+					/* Throw away data */
+					REG(I2C_DRR);
+				}
+				if (!(stat & (I2C_STAT_ARDY | I2C_STAT_SCD)))
+					continue;
+				REG(I2C_CON) |= I2C_CON_STP;
+				printf("%s: nack %x %x\n", __func__, stat, sum_stat);
+				return -1;
+			}
+			return sum_stat;
 		}
 	}
 
 	REG(I2C_STAT) = 0xffff;
-	return(stat | I2C_TIMEOUT);
+	if (stop) {
+		REG(I2C_CON) |= I2C_CON_STP;
+		printf("%s: stop timeout %x %x\n", __func__, stat, sum_stat);
+	} else {
+		printf("%s: timeout, trying i2c reset %x %x\n", __func__, stat, sum_stat);
+		i2c_init(CFG_I2C_SPEED, CFG_I2C_SLAVE);
+		if (DoGpioFreeBus() == 1) {
+			printf("%s: toggling scl freed bus\n", __func__);
+		}
+	}
+	return -1;
 }
 
 
@@ -98,10 +213,8 @@ void i2c_init(int speed, int slaveadd)
 {
 	u_int32_t	div, psc;
 
-	if (REG(I2C_CON) & I2C_CON_EN) {
-		REG(I2C_CON) = 0;
-		udelay (50000);
-	}
+	REG(I2C_CON) = 0;
+	udelay (50000);
 
 	psc = 2;
 	div = (CFG_PERIPHERAL_CLOCK / ((psc + 1) * speed)) - 10;	/* SCLL + SCLH */
@@ -160,7 +273,7 @@ int i2c_probe(u_int8_t chip)
 
 int i2c_read(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 {
-	u_int32_t	tmp;
+	int tmp;
 	int		i;
 
 	if ((alen < 0) || (alen > 2)) {
@@ -177,9 +290,9 @@ int i2c_read(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 		REG(I2C_SA) = chip;
 		REG(I2C_CON) = tmp;
 
-		tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK);
+		tmp = poll_i2c_irq(I2C_STAT_XRDY);
+		if (tmp < 0) return 1;
 
-		CHECK_NACK();
 
 		switch (alen) {
 			case 2:
@@ -191,9 +304,8 @@ int i2c_read(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 					return(1);
 				}
 
-				tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK);
-
-				CHECK_NACK();
+				tmp = poll_i2c_irq(I2C_STAT_XRDY);
+				if (tmp < 0) return 1;
 				/* No break, fall through */
 			case 1:
 				/* Send address LSByte */
@@ -204,14 +316,8 @@ int i2c_read(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 					return(1);
 				}
 
-				tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK | I2C_STAT_ARDY);
-
-				CHECK_NACK();
-
-				if (!(tmp & I2C_STAT_ARDY)) {
-					REG(I2C_CON) = 0;
-					return(1);
-				}
+				tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_ARDY);
+				if (tmp < 0) return 1;
 		}
 	}
 
@@ -222,23 +328,29 @@ int i2c_read(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 	REG(I2C_CON) = tmp;
 
 	for (i = 0; i < len; i++) {
-		tmp = poll_i2c_irq(I2C_STAT_RRDY | I2C_STAT_NACK | I2C_STAT_ROVR);
-
-		CHECK_NACK();
+		tmp = poll_i2c_irq(I2C_STAT_RRDY | I2C_STAT_ROVR);
+		if (tmp < 0) {
+			printf("%s: read %i bytes a\n", __func__, i);
+			return 1;
+		}
 
 		if (tmp & I2C_STAT_RRDY) {
 			buf[i] = REG(I2C_DRR);
 		} else {
+			printf("%s: read %i bytes b\n", __func__, i);
 			REG(I2C_CON) = 0;
 			return(1);
 		}
 	}
 
-	tmp = poll_i2c_irq(I2C_STAT_SCD | I2C_STAT_NACK);
-
-	CHECK_NACK();
+	tmp = poll_i2c_irq(I2C_STAT_SCD);
+	if (tmp < 0) {
+		printf("%s: read %i bytes c\n", __func__, i);
+		return 1;
+	}
 
 	if (!(tmp & I2C_STAT_SCD)) {
+		printf("%s: read %i bytes d\n", __func__, i);
 		REG(I2C_CON) = 0;
 		return(1);
 	}
@@ -277,9 +389,8 @@ int i2c_write(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 	switch (alen) {
 		case 2:
 			/* Send address MSByte */
-			tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK);
-
-			CHECK_NACK();
+			tmp = poll_i2c_irq(I2C_STAT_XRDY);
+			if (tmp < 0) return 1;
 
 			if (tmp & I2C_STAT_XRDY) {
 				REG(I2C_DXR) = (addr >> 8) & 0xff;
@@ -290,9 +401,8 @@ int i2c_write(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 			/* No break, fall through */
 		case 1:
 			/* Send address LSByte */
-			tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK);
-
-			CHECK_NACK();
+			tmp = poll_i2c_irq(I2C_STAT_XRDY);
+			if (tmp < 0) return 1;
 
 			if (tmp & I2C_STAT_XRDY) {
 				REG(I2C_DXR) = addr & 0xff;
@@ -303,9 +413,8 @@ int i2c_write(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 	}
 
 	for (i = 0; i < len; i++) {
-		tmp = poll_i2c_irq(I2C_STAT_XRDY | I2C_STAT_NACK);
-
-		CHECK_NACK();
+		tmp = poll_i2c_irq(I2C_STAT_XRDY);
+		if (tmp < 0) return 1;
 
 		if (tmp & I2C_STAT_XRDY) {
 			REG(I2C_DXR) = buf[i];
@@ -314,9 +423,8 @@ int i2c_write(u_int8_t chip, u_int32_t addr, int alen, u_int8_t *buf, int len)
 		}
 	}
 
-	tmp = poll_i2c_irq(I2C_STAT_SCD | I2C_STAT_NACK);
-
-	CHECK_NACK();
+	tmp = poll_i2c_irq(I2C_STAT_SCD);
+	if (tmp < 0) return 1;
 
 	if (!(tmp & I2C_STAT_SCD)) {
 		REG(I2C_CON) = 0;
